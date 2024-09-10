c548708db0a1da3c1ce0fa862a9422ce73fb2a2a
Jack Fretwell <jackfretwell99@icloud.com>
2024-09-11 00:02:29 +0100

    playRound function working correctly

--- index.js
+++ index.js
@@ -27,5 +27,38 @@ function getHumanChoice(){
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    const humansChoice = humanChoice.toLowerCase();
+    if(humansChoice == computerChoice){
+        console.log("You drew!");
+    }
+    else if(humansChoice == "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    }
+    else if(humansChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Paper");
+        humanScore++;
+    }
+    else if(humansChoice == "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+    else if(humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+    else{
+        console.log("You win! Scissors beats Rock");
+        humanScore++;
+    }
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+playRound(humanSelection, computerSelection);
